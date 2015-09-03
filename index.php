09c7a04f4e4d582b6591855ac6244a08ca71aa54
Ott <otterik@Otts-MacBook-Pro.local>
2015-09-03 16:24:24 +0300

    Tutvustus [Delivers #102652600]

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tutvustus</title>
+    <style>
+        h1 {
+            text-align: center;
+    </style>
+    <style>
+        p {
+            text-align: left;
+    </style>
+</head>
+<body>
+
+<h1>Martin Kooga tutvustus</h1>
+<!--Martin iseloomustab ennast selliste sõnadega-->
+<p>Jõhvist pärit küünik</p>
+
+<a href="www.google.ee">www.google.ee</a>
+
+</body>
+</html>
