bffd9d10795859dc2f0af9f77fda695cff0857f2
otteendra <ott-erik.eendra@khk.ee>
2015-09-09 11:39:29 +0300

    Tutvustus [Delivers #102652600]

--- index.php
+++ index.php
@@ -1,24 +1,64 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <link type="text/css" rel="stylesheet" href="stylesheet.css"/>
     <meta charset="UTF-8">
     <title>Tutvustus</title>
-    <style>
-        h1 {
-            text-align: center;
-    </style>
-    <style>
-        p {
-            text-align: left;
-    </style>
+
 </head>
 <body>
 
-<h1>Martin Kooga tutvustus</h1>
+
+<h1>Martin Kooga</h1>
 <!--Martin iseloomustab ennast selliste sõnadega-->
 <p>Jõhvist pärit küünik</p>
+<img src=assets/Snapshot_20120212_5.JPG height="440" width="580">
 
-<a href="www.google.ee">www.google.ee</a>
+<div>
+    <h2>Lühitutvustus</h2>
+    <p>
+        Martin on õppinud 3 aastat Tartu Ülikoolis materjaliteadust, kuid kooli lõpetada ei ole tal veel õnnestunud.<br>
+        Ta on suur ajaloohuviline, kelle põhilisteks ajaviideteks on arvutimängud, filmid, sarjad ning raamatud.
+    </p>
+</div>
+
+<div>
+<h2>Iseloomuomadused</h2>
+<ul>
+    <li>Küünik</li>
+    <li>Skeptik</li>
+    <li>Pessimist</li>
+</ul>
+</div>
+<div>
+<h2>Linnad kuhu sooviks reisida</h2>
+<ol>
+    <li>Tokyo</li>
+    <li>New York</li>
+    <li>London</li>
+</ol>
+</div>
+
+<table>
+    <tr>
+        <th>Sünniaeg</th>
+        <th>Vanus</th>
+        <th>Sünnilinn</th>
+    </tr>
+    <tr>
+        <td>11.11.1111</td>
+        <td>00</td>
+        <td>Jõhvi</td>
+    </tr>
+
+</table>
+
+
+
+
+<hr>
+
+<a href="www.khk.ee">Tartu Kutsehariduskeskus</a>
 
 </body>
 </html>
